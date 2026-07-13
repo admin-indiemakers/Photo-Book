d2039754413bbe03c5f2165080a028a880ca9e1d
4dithyan <mailforadithyan@gmail.com>
2026-07-13 20:04:14 +0530

    fixed build error

--- editor/src/lib/prisma.ts
+++ editor/src/lib/prisma.ts
@@ -1,11 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { _prisma?: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
+export const getPrisma = () => {
+  if (!globalForPrisma._prisma) {
+    globalForPrisma._prisma = new PrismaClient({
+      log: ['query'],
+    });
+  }
+  return globalForPrisma._prisma;
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = new Proxy({} as PrismaClient, {
+  get: (target, prop) => {
+    if (prop === 'then') return undefined;
+    const client = getPrisma();
+    const value = client[prop as keyof PrismaClient];
+    return typeof value === 'function' ? value.bind(client) : value;
+  }
+});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma._prisma = getPrisma();
